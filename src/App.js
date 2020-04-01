b835306c5f7a0ae1ee178b21e52de578bed31f3a
Lee Chuanfeng <lee_chuanfeng@outlook.com>
2020-04-02 00:59:00 +0800

    Added reach router, logo redirects to homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 //import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import { Router, Link } from '@reach/router';
+import Details from "./Details";
 
 const App = () => {
   // return React.createElement("div", { id: "something-important" }, [
@@ -25,8 +27,13 @@ const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to ="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/"/>
+          <Details path="/details/:id"/>
+        </Router>
       </div>
     </React.StrictMode>
   );
