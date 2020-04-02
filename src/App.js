44cc8df0aa444c793a5d51386f60766cfcd127c5
Lee Chuanfeng <lee_chuanfeng@outlook.com>
2020-04-02 18:00:27 +0800

    Added theme context, added route to pet, code clean up

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 //import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import { Router, Link } from '@reach/router';
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]);
+  const themeHook = useState('darkblue');
+
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to ="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/"/>
-          <Details path="/details/:id"/>
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to ="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/"/>
+            <Details path="/details/:id"/>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
